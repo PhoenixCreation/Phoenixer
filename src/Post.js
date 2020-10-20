e06677ecc4bb1e36e47c1e86818b5736a1b148c5
phoenix <phs28052001@gmail.com>
2020-10-20 12:31:05 +0530

    Added like animation

--- src/Post.js
+++ src/Post.js
@@ -16,6 +16,16 @@ function Post({postID, user }) {
   const [commentText,setCommentText] = useState('')
   const [liked,setLiked] = useState(false)
   const [likeCount,setLikeCount] = useState(0)
+  const [likeState,setLikeState] = useState(false)
+
+  useEffect(() => {
+    if(liked){
+      setLikeState(true)
+    }
+    else{
+      setLikeState(false)
+    }
+  },[liked])
 
   useEffect(() => {
     db.collection('posts').doc(postID).onSnapshot((doc) => {
@@ -39,11 +49,13 @@ function Post({postID, user }) {
     db.collection('posts').doc(postID).get().then((doc) => {
       var data = doc.data().likes
       if(data.includes(user.displayName)){
+        //Unliked
         db.collection('posts').doc(postID).update({
           likes: firebase.firestore.FieldValue.arrayRemove(user.displayName)
         })
       }
       else{
+        //liked
         db.collection('posts').doc(postID).update({
           likes: firebase.firestore.FieldValue.arrayUnion(user.displayName)
         })
@@ -96,7 +108,19 @@ function Post({postID, user }) {
         <Avatar src={postInfo.userAvatar} alt={postInfo.username} className="post__headerAvatar" />
         <Link to={"user/"+postInfo.username} ><h3>{postInfo.username}</h3></Link>
       </div>
-      <img className="post__image" src={postInfo.imageURL} alt="Some beautiful movements" onDoubleClick={toggleLike} />
+      <div className="post__imageSection">
+        <img className="post__image" src={postInfo.imageURL} alt="Some beautiful movements" onDoubleClick={toggleLike} />
+        <div className={
+          `
+          post__imageLikeAnimation
+          ${likeState ? "animate_like" : ""}
+          `
+        }><svg class="heart" viewBox="0 0 32 29.6">
+  <path d="M23.6,0c-3.4,0-6.3,2.7-7.6,5.6C14.7,2.7,11.8,0,8.4,0C3.8,0,0,3.8,0,8.4c0,9.4,9.5,11.9,16,21.2
+	c6.1-9.3,16-12.1,16-21.2C32,3.8,28.2,0,23.6,0z"/>
+</svg>
+</div>
+      </div>
       <div className="post__likesection">
       {user?.displayName && liked ? (
         <IconButton onClick={toggleLike}>
