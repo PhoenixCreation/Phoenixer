2cc26ed09d7fb48dafab257cd5f67c87cc457635
phoenix <phs28052001@gmail.com>
2021-01-09 10:40:12 +0530

    First Build

---
+++ build/precache-manifest.4d9c0011ea3db0c9f3c5128e8b4e1177.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82a10207c344ba76ce6ea4a2aa4677fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f8e192b7b714b1e1c35",
+    "url": "/static/css/main.651cd99d.chunk.css"
+  },
+  {
+    "revision": "31eb0c47a4ba1c7f3f32",
+    "url": "/static/js/2.37f84f1d.chunk.js"
+  },
+  {
+    "revision": "03baea06b30de78f9cea787387a41b90",
+    "url": "/static/js/2.37f84f1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f8e192b7b714b1e1c35",
+    "url": "/static/js/main.964ec5cd.chunk.js"
+  },
+  {
+    "revision": "ae040155ea3d7c8d3b75",
+    "url": "/static/js/runtime-main.379ecfdd.js"
+  }
+]);
